b5ae63122e6d289c1f835e3f065053548e60ff9a
Oleksandr <alexandrenko3006@gmail.com>
2026-03-11 12:45:32 +0200

    Added roles and fixed access for course-matireals.

--- buero-backend-api/src/modules/course-materials/course-materials.module.ts
+++ buero-backend-api/src/modules/course-materials/course-materials.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { CourseMaterialService } from './course-material.service';
 import { CourseMaterialsController } from './course-materials.controller';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule, UserModule],
   controllers: [CourseMaterialsController],
   providers: [CourseMaterialService],
   exports: [CourseMaterialService],
--- buero-backend-api/src/modules/courses/courses.module.ts
+++ buero-backend-api/src/modules/courses/courses.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
+import { UserModule } from '../user/user.module';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { CourseService } from './course.service';
 import { CoursesController } from './courses.controller';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule, UserModule],
   controllers: [CoursesController],
   providers: [CourseService],
   exports: [CourseService],
