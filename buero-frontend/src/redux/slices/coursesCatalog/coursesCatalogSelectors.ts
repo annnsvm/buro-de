c946a22d721f2cc3f3f46d4663e288fa45c9a5ee
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2026-03-17 07:31:26 +0200

    Revert "Update coursesCatalogSelectors.ts"
    This reverts commit 42a02b33598d7ba838563ee3fd7e820ae26feb2f.

--- buero-frontend/src/redux/slices/coursesCatalog/coursesCatalogSelectors.ts
+++ buero-frontend/src/redux/slices/coursesCatalog/coursesCatalogSelectors.ts
@@ -2,17 +2,4 @@ import type { RootState } from '../../store';
 
 export const selectCoursesCatalogItems = (state: RootState) => state.coursesCatalog.items;
 export const selectCoursesCatalogStatus = (state: RootState) => state.coursesCatalog.status;
-export const selectCoursesCatalogIsLoading = (state: RootState) =>
-  state.coursesCatalog.status === 'loading';
 export const selectCoursesCatalogFilters = (state: RootState) => state.coursesCatalog.filters;
-export const selectCoursesCatalogPage = (state: RootState) => state.coursesCatalog.page;
-export const selectCoursesCatalogPageSize = (state: RootState) => state.coursesCatalog.pageSize;
-export const selectCoursesCatalogTotalCount = (state: RootState) =>
-  state.coursesCatalog.totalCount;
-export const selectCoursesCatalogError = (state: RootState) => state.coursesCatalog.error;
-
-// export const selectSelectedCourse = (state: RootState) => state.coursesCatalog.selectedCourse;
-// export const selectSelectedCourseStatus = (state: RootState) =>
-//   state.coursesCatalog.selectedCourseStatus;
-// export const selectSelectedCourseError = (state: RootState) =>
-//   state.coursesCatalog.selectedCourseError;
