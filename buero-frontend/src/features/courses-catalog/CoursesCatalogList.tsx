aae9a4833a518a67c9b23407ebb3c3888c6b7749
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2026-03-17 07:18:38 +0200

    fetch from redux and connect with modal
    ADDED: fetch from redux and connect with modal

--- buero-frontend/src/features/courses-catalog/CoursesCatalogList.tsx
+++ buero-frontend/src/features/courses-catalog/CoursesCatalogList.tsx
@@ -1,16 +1,15 @@
 import { Container, Section } from '@/components/layout';
 import CourseCard from './CourseCard';
-import type { CourseCardProps } from './CourseCard';
+import type { CoursesCatalogListProps } from '@/types/features/courses-catalog/CoursesCatalogList.types';
 
-type CoursesGridProps = {
-  courses: CourseCardProps[];
-};
-
-const CoursesCatalogList = ({ courses }: CoursesGridProps) => {
+const CoursesCatalogList = ({ courses }: CoursesCatalogListProps) => {
   return (
-    <Section className="bg-white py-12">
-      <Container className="md:px-20">    
-        <ul className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3">
+    <Section className="bg-white pb-28">
+      <Container className="md:px-0">    
+      <ul className="grid gap-8 grid-cols-[repeat(auto-fit,minmax(303px,1fr))] justify-items-center">
+
+        {/* <ul className="grid grid-cols-1 justify-centr sm:gap-x-4 md:gap-x-8 gap-y-16 sm:grid-cols-2 md:grid-cols-3"> */}
+        {/* <ul className="flex flex-wrap justify-center sm:gap-x-4 md:gap-x-8 gap-y-16 "> */}
           {courses.map((course) => (
             <li key={course.id}> 
               <CourseCard {...course} />
