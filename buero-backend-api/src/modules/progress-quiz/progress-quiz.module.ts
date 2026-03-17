442d1ac954e0ccad46689bc57bb1d742301ac286
Anna Samoilova <121616319+annnsvm@users.noreply.github.com>
2026-03-17 12:42:33 +0100

    revert changes in backend module

--- buero-backend-api/src/modules/progress-quiz/progress-quiz.module.ts
+++ buero-backend-api/src/modules/progress-quiz/progress-quiz.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
-import { UserModule } from '../user/user.module';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { ProgressService } from './progress.service';
 import { QuizService } from './quiz.service';
@@ -9,7 +8,7 @@ import { CourseProgressController } from './course-progress.controller';
 import { QuizController } from './quiz.controller';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UserModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [
     ProgressController,
     CourseProgressController,
